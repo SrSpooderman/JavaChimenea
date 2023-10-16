849c4591e58c19af317ea17031392b2bad45510f
Francisco Valencia <pfrank123456789@hotmail.com>
2023-10-16 19:20:01 +0200

    Limpiando codigo

--- prueba.java
+++
@@ -1,9 +0,0 @@
-public class prueba {
-    public static void main (String args[]){
-        Temperature temp = new Temperature(200,200,0.09,0.055);
-        temp.next();
-        temp.show();
-        temp.next();
-        temp.show();
-    }
-}
